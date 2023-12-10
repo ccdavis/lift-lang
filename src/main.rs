1407e2c0a080f32f74a389b7398471468e9162c5
Colin Davis <ccd.umn.edu>
2023-12-09 19:45:56 -0600

    format

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub grammar); // synthesized by LALRPOP
 
 #[test]
-fn calculator1_test() {
-}
+fn calculator1_test() {}
 
 fn main() {
- println!("Hello world!")
+    println!("Hello world!")
 }
-
--- src/syntax.rs
+++ src/syntax.rs
@@ -17,38 +17,60 @@ arg-list := EPSILON
           | <expr> ("," <expr
 */
 #![allow(unused_variables)]
-#[derive(Clone,Debug,PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Operator {
-    Div, Mul, Add, Sub, Gt, Lt, Gte, Lte, Eq, Neq, And, Or, Not
+    Div,
+    Mul,
+    Add,
+    Sub,
+    Gt,
+    Lt,
+    Gte,
+    Lte,
+    Eq,
+    Neq,
+    And,
+    Or,
+    Not,
 }
 
-#[derive(Clone,Debug,PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Param {
     pub name: String,
     pub data_type: DataType,
 }
 
-#[derive(Clone,Debug,PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum DataType {
-    Str, Int, Flt, Bool,  Any, None,
-    Map { key_type: Box<DataType>, value_type: Box<DataType>}, 
-    List { element_type: Box<DataType> },
+    Str,
+    Int,
+    Flt,
+    Bool,
+    Any,
+    None,
+    Map {
+        key_type: Box<DataType>,
+        value_type: Box<DataType>,
+    },
+    List {
+        element_type: Box<DataType>,
+    },
     Set(Box<DataType>),
-    Struct(Vec<Param>),    
+    Struct(Vec<Param>),
 }
 
-#[derive(Clone,Debug,PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct KeywordArg {
     pub name: String,
     pub value: Expr,
 }
 
-#[derive(Clone,Debug,PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum LiteralData {
     Int(i64),
     Flt(f64),
     Str(String),
-    Bool(bool),    
+    Bool(bool),
 }
 
 impl From<&str> for LiteralData {
@@ -62,7 +84,6 @@ impl From<String> for LiteralData {
         LiteralData::Str(data.clone())
     }
 }
-
 
 impl From<i64> for LiteralData {
     fn from(data: i64) -> LiteralData {
@@ -82,44 +103,90 @@ impl From<bool> for LiteralData {
     }
 }
 
-
-
-#[derive(Clone,Debug,PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum Expr {
-    Literal(LiteralData),    
-    ListLiteral(Vec<Expr>),    
-    MapLiteral(Vec<(Expr,Expr)>),    
+    Literal(LiteralData),
+    ListLiteral(Vec<Expr>),
+    MapLiteral(Vec<(Expr, Expr)>),
     Block(Vec<Expr>),
-    BinaryExpr { left: Box<Expr>, op: Operator, right: Box<Expr> },
-    UnaryExpr {op: Operator, expr: Box<Expr> },
+    BinaryExpr {
+        left: Box<Expr>,
+        op: Operator,
+        right: Box<Expr>,
+    },
+    UnaryExpr {
+        op: Operator,
+        expr: Box<Expr>,
+    },
     Variable(String),
-    Call { fn_name: String, args: Vec<KeywordArg>},
-     DefineFunction { fn_name: String, params: Vec<Param>, return_type: DataType, body: Box<Expr>},
-    Lambda { args: Vec<KeywordArg>, return_type: DataType, body: Box<Expr> },
-    Let { var_name: String, data_type: DataType, value: Box<Expr>},
-    DefineType{ type_name: String, definition: DataType },
-    If { cond: Box<Expr>, then: Box<Expr>, elsif: Vec<Box<Expr>>, final_else: Box<Expr>},
-    Match { cond: Box<Expr>, against: Vec<(Expr,Box<Expr>)>},
-    While { cond: Box<Expr>, body: Box<Expr>},
+    Call {
+        fn_name: String,
+        args: Vec<KeywordArg>,
+    },
+    DefineFunction {
+        fn_name: String,
+        params: Vec<Param>,
+        return_type: DataType,
+        body: Box<Expr>,
+    },
+    Lambda {
+        args: Vec<KeywordArg>,
+        return_type: DataType,
+        body: Box<Expr>,
+    },
+    Let {
+        var_name: String,
+        data_type: DataType,
+        value: Box<Expr>,
+    },
+    DefineType {
+        type_name: String,
+        definition: DataType,
+    },
+    If {
+        cond: Box<Expr>,
+        then: Box<Expr>,
+        elsif: Vec<Box<Expr>>,
+        final_else: Box<Expr>,
+    },
+    Match {
+        cond: Box<Expr>,
+        against: Vec<(Expr, Box<Expr>)>,
+    },
+    While {
+        cond: Box<Expr>,
+        body: Box<Expr>,
+    },
     Return(Box<Expr>),
 }
 
 impl Expr {
     pub fn add(l: Expr, r: Expr) -> Expr {
-        Expr::BinaryExpr{left: Box::new(l), right: Box::new(r), op: Operator::Add}
+        Expr::BinaryExpr {
+            left: Box::new(l),
+            right: Box::new(r),
+            op: Operator::Add,
+        }
     }
     pub fn sub(l: Expr, r: Expr) -> Expr {
-        Expr::BinaryExpr{left: Box::new(l), right: Box::new(r), op: Operator::Sub}
+        Expr::BinaryExpr {
+            left: Box::new(l),
+            right: Box::new(r),
+            op: Operator::Sub,
+        }
     }
     pub fn mul(l: Expr, r: Expr) -> Expr {
-        Expr::BinaryExpr{left: Box::new(l), right: Box::new(r), op: Operator::Mul}
+        Expr::BinaryExpr {
+            left: Box::new(l),
+            right: Box::new(r),
+            op: Operator::Mul,
+        }
     }
     pub fn div(l: Expr, r: Expr) -> Expr {
-        Expr::BinaryExpr{left: Box::new(l), right: Box::new(r), op: Operator::Div}
+        Expr::BinaryExpr {
+            left: Box::new(l),
+            right: Box::new(r),
+            op: Operator::Div,
+        }
     }
-
-
-
-
-
 }
