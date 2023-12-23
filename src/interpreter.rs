731de08e37bf6a3b06ef979e995e58b2e7150fe6
Colin Davis <ccd.umn.edu>
2023-12-22 20:05:18 -0600

    updated grammar template to produce the right syntax

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,28 +1,38 @@
 use crate::symboltable::Env;
 use crate::syntax::Expr;
+use crate::syntax::*;
+
+
+pub fn add_index(envr: &mut Env, e: &mut Expr) {
+    match e {
+        Expr::DefineFunction(_) => {
+            
+        },
+        
+        
+        Expr::Let { name, value, index } => {
+            envr.data.push(value.clone());
+            let new_index = envr.data.size();
+            index = new_index;
+            envr.name[index] = name.clone();
+            envr.index[name.clone()] = index
+        }
+        
+        
+        
+        _ => (),
+    }
+
+}
+
+
 
 impl Expr {
     pub fn interpret(&self, symbols: &mut Env) {
 
     }
 
-    pub fn add_index(envr: &mut Env, expr: &mut Expr) {
-        match expr {
-            Expr::Let { name, value, index } => {
-                envr.data.push(value.clone());
-                let new_index = envr.data.size();
-                index = new_index;
-                envr.name[index] = name.clone();
-                envr.index[name.clone()] = index
-            }
-            Expr::
-            
-            
-            _ => (),
-        }
-
-    }
-
+    
     pub fn index_symbols(&mut self) {
         let mut vars = 0;
         match self {            
