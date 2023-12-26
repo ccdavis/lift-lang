716007b5421d0020485aaa41c3dad359e1600fdf
Colin Davis <ccd@umn.edu>
2023-12-26 16:25:04 -0600

    Working top-down to interpret code

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,8 +3,14 @@ use crate::semantic_analysis::*;
 use crate::symboltable::SymbolTable;
 use crate::syntax::Expr;
 
+// TODO this should eventually  store line numbers, columns in source and function names
+struct RuntimeError {
+    stack: Vec<String>,
+    msg: String,
+}
+
 impl Expr {
-    pub fn interpret(&mut self, symbols: &mut SymbolTable) -> Result<(), Vec<ParseError>> {
+    pub fn prepare(&mut self, symbols: &mut SymbolTable) -> Result<(), Vec<ParseError>> {
         let mut errors = Vec::new();
 
         // Analyze  parse tree to index symbols across scopes.
@@ -20,5 +26,32 @@ impl Expr {
         } else {
             Err(errors)
         }
+    }
+
+    // Receives a "prepared" parse tree and symbol table.
+    pub fn interpret(&self, symbols: &mut SymbolTable) -> Result<Option<Expr>,RuntimeError> {
+        match self {
+            Expr::Program{ body, environment} =>{
+                let mut eval_to: Option<Expr> = None;
+                let mut expressions =  body.iter().peekable();
+                while let Some(expression) = expressions.next() {
+                    let tmp_eval_to = match expression.interpret(symbols) {
+                        Ok(maybe_expr)=> maybe_expr,
+                        Err(rt) => {
+                            eprintln!("Runtime error: {}",rt.msg);                            
+                            return Err(rt);
+                        }
+
+                    };
+                    if expressions.peek().is_none() {
+                        eval_to = tmp_eval_to
+                    }
+
+                }
+                Ok(eval_to)
+            },
+            _ => Ok(()),
+        }
+                
     }
 } // impl
